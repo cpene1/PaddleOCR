68f9b43d884dc10f163831c227612b886cbc7a2e
Cosmin Pene <pene.cosmin.octavian@gmail.com>
2021-08-19 14:13:24 +0200

    updated to initial clean ppocr_vts version

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import paddleocr
+#import paddleocr
 from .paddleocr import *
 
 __version__ = paddleocr.VERSION
